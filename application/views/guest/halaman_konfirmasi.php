ce847571d5f2a21ff6eb7aa7ce98df49b3c53658
jefri19 <mborgud46@gmail.com>
2020-08-09 09:02:28 +0700

    Update config.php and halaman_konfirmasi.php

--- application/views/guest/halaman_konfirmasi.php
+++ application/views/guest/halaman_konfirmasi.php
@@ -1,6 +1,24 @@
-<form action="POST">
-    <div class="form-grou">
-        <label>Kode Boking</label>
-        <input type="text" class="form-control" placeholder="Kode Boking">
+<div class="container-fluid">
+    <div class="row justify-content-center my-5">
+        <div class="col-md-5 ">
+            <div class="card">
+                <div class="card-header bg-dark text-white text-center">
+                   <strong>Konfirmasi Pembayaran</strong> 
+                </div>
+                <div class="card-body">
+                    <form action="" method="POST">
+
+                     <div class="form-grou">
+                        <label>Kode Boking</label>
+                        <input type="text" class="form-control">
+                        </div><br>                       
+                        <div class="btn btn-dark"><button>Cek Kode Boking</button>
+                     </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-</form>
+</div>
+
